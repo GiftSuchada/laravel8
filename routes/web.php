ca378995be3e6dd150178e62a575d7cc3dbfd3ce
GiftSuchada <suchada.nuam@vru.ac.th>
2022-08-25 12:32:33 +0700

    week 7.2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Covid19Controller;
 use App\Http\Controllers\MyProfileController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
@@ -174,3 +175,6 @@ Route::post("/staff", [StaffController::class, "store"]);
 Route::patch("/staff/{id}", [StaffController::class, "update"]);
 
 Route::delete('/staff/{id}', [StaffController::class, 'destroy']);
+
+//Route::resource('post', 'PostController');
+Route::resource('post', PostController::class);
